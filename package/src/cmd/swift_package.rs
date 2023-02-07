cd0fb9b20c5191c39d2866da8d96b52609b21ae6
hakesson <henrik@akesson.mobi>
2023-02-07 19:13:28 +0100

    Add swift package headers search path

--- package/src/cmd/swift_package.rs
+++ package/src/cmd/swift_package.rs
@@ -26,7 +26,8 @@ let package = Package(
 		),
 		.target(
 			name: "{name}Lib",
-			dependencies: ["{name}"]
+			dependencies: ["{name}"],
+			cxxSettings: [.headerSearchPath("Headers")]
 		)
 	]
 )
