055068f3eb0633369205b097e183e81c840ac93a
hakesson <henrik@akesson.mobi>
2023-02-07 09:00:57 +0100

    New Xcode multiplatform project

---
+++ example/apple/uniffi-package/uniffi_packageApp.swift
@@ -0,0 +1,17 @@
+//
+//  uniffi_packageApp.swift
+//  uniffi-package
+//
+//  Created by Henrik Akesson on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct uniffi_packageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
